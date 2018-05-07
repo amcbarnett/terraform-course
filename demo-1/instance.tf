e8382004220c5ef3ee01976139700e85d0078ddd
amcbarnett <8419906+amcbarnett@users.noreply.github.com>
2018-05-07 14:33:24 -0400

    Changed Instance Name

--- demo-1/instance.tf
+++ demo-1/instance.tf
@@ -2,6 +2,6 @@ resource "aws_instance" "example" {
   ami           = "${lookup(var.AMIS, var.AWS_REGION)}"
   instance_type = "${var.AWS_INSTANCE_TYPE}"
   tags {
-    Name = "ancil-demo1-example"
+    Name = "ancil-demo1-comcast"
   } 
 }
